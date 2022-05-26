f97fe7f2c2e810b53949d6b34e11706979bdbd68
Max Santiago <mxxsant@hotmail.com>
2022-05-25 22:49:14 -0700

    feat: created constant variables

---
+++ configuration/constants.h
@@ -0,0 +1,14 @@
+#ifndef UNTITLED7_CONSTANTS_H
+
+#define USERNAME_LENGTH 20
+#define PASSWORD_LENGTH 15
+#define DATABASE "libraryDB.bin"
+
+#define BOOK_NAME_LENGTH 50
+#define AUTHOR_LENGTH 50
+#define CLIENT_LENGTH 50
+#define FILE_SIZE sizeof(Credentials)
+
+#define UNTITLED7_CONSTANTS_H
+
+#endif
